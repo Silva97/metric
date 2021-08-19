5a43e7c3c75057da3a319f93a3f5e5e44083e9c3
Luiz Felipe <felipe.silva337@yahoo.com>
2021-08-19 17:23:33 -0300

    refactor: improve project to make framework structure

---
+++ configuration.cmake
@@ -0,0 +1,6 @@
+set(BINARY "output_bin")    # The output binary name
+
+# The subdirectories of "src/" to find source code
+# Use ${ARCH} variable to access architecture dependent directory
+# Example: "arch/${ARCH}/subdir"
+set(SRCDIRS #[["sub1" "sub2" "sub3"]])
